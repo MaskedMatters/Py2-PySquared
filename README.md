5b1c76a8681f7cea606126002f63ad7b63601df3
MaskedMatters <Masked.Matters@gmail.com>
2023-08-30 20:32:37 -0400

    Adding more to the lexer and changing readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Py2-PySquared
- This is a new programming language made my Liam, Sebastian, and I. It is based off of Python and will be coded in that. We are aiming for an interpreted language but we'll see what'll happen.
+This is a new programming language made by Liam, Sebastian, and I. It is based off of Python and will be coded in that. We are aiming for an interpreted language but we'll see what'll happen.
--- lexer.py
+++ lexer.py
@@ -1,3 +1,5 @@
+DIGITS = '0123456789'
+
 
 TT_INT    = 'TT_INT'
 TT_FLOAT  = 'FLOAT'
@@ -7,6 +9,7 @@ TT_MUL    = 'MUL'
 TT_DIV    = 'DIV'
 TT_LPAREN = 'LPAREN'
 TT_RPAREN = 'RPAREN'
+
 
 class Token:
     
@@ -18,12 +21,64 @@ class Token:
         if self.value: return f'{self.type}:{self.value}'
         return f'{self.type}'
 
+
 class Lexer:
     def __init__(self, text):
         self.text = text
         self.pos = -1
         self.current_char = None
+        self.advance()
 
     def advance(self):
         self.pos += 1
         self.current_char = self.text[pos] if self.pos < len(self.text) else None
+
+    def make_tokens(self):
+        tokens = []
+
+        while self.current_char != None:
+            if self.current_char in ' \t':
+                self.advance()
+            elif self.current_char in DIGITS:
+                tokens.append(self.make_number())
+            elif self.current_char == '+':
+                tokens.append(Token(TT_PLUS))
+                self.advance
+            if self.current_char in ' \t':
+                self.advance()
+            elif self.current_char == '-':
+                tokens.append(Token(TT_MINUS))
+                self.advance
+            if self.current_char in ' \t':
+                self.advance()
+            elif self.current_char == '*':
+                tokens.append(Token(TT_MUL))
+                self.advance
+            if self.current_char in ' \t':
+                self.advance()
+            elif self.current_char == '/':
+                tokens.append(Token(TT_DIV))
+                self.advance
+            if self.current_char in ' \t':
+                self.advance()
+            elif self.current_char == '(':
+                tokens.append(Token(TT_LPAREN))
+                self.advance
+            if self.current_char in ' \t':
+                self.advance()
+            elif self.current_char == ')':
+                tokens.append(Token(TT_RPAREN))
+                self.advance
+        return tokens
+
+    def make_number(self):
+        num_str = ''
+        dot_count = 0
+
+        while self.current_char != None and self.current_char in DIGITS + '.':
+            if self.current_char == '.':
+                if dot_count == 1: break
+                dot_count += 1
+                num_str += '.'
+            else:
+                num_str += self.current_char
