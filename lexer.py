47016c1082fa6ef305a620c16db06d9bdb19f856
N30nyx <120945114+Neonyx27@users.noreply.github.com>
2023-09-04 17:13:50 -0400

    Finished lexer and added shell.py

--- lexer.py
+++ lexer.py
@@ -1,5 +1,23 @@
+#CONSTANTS
+
 DIGITS = '0123456789'
 
+#ERRORS
+
+class Error:
+    def __init__(self, error_name, details):
+        self.error_name = error_name
+        self.details = details
+
+    def as_string(self):
+        result = f'{self.error_name}: {self.details}'
+        return result
+    
+class IllegalCharError(Error):
+    def __init__(self, details):
+        super().__init__('Illegal Character', details)
+
+#TOKENS
 
 TT_INT    = 'TT_INT'
 TT_FLOAT  = 'FLOAT'
@@ -13,7 +31,7 @@ TT_RPAREN = 'RPAREN'
 
 class Token:
     
-    def __init__(self, type_, value):
+    def __init__(self, type_, value=None):
         self.type = type_
         self.value = value
 
@@ -21,6 +39,7 @@ class Token:
         if self.value: return f'{self.type}:{self.value}'
         return f'{self.type}'
 
+#LEXER
 
 class Lexer:
     def __init__(self, text):
@@ -31,7 +50,7 @@ class Lexer:
 
     def advance(self):
         self.pos += 1
-        self.current_char = self.text[pos] if self.pos < len(self.text) else None
+        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
 
     def make_tokens(self):
         tokens = []
@@ -43,33 +62,28 @@ class Lexer:
                 tokens.append(self.make_number())
             elif self.current_char == '+':
                 tokens.append(Token(TT_PLUS))
-                self.advance
-            if self.current_char in ' \t':
                 self.advance()
             elif self.current_char == '-':
                 tokens.append(Token(TT_MINUS))
-                self.advance
-            if self.current_char in ' \t':
                 self.advance()
             elif self.current_char == '*':
                 tokens.append(Token(TT_MUL))
-                self.advance
-            if self.current_char in ' \t':
                 self.advance()
             elif self.current_char == '/':
                 tokens.append(Token(TT_DIV))
-                self.advance
-            if self.current_char in ' \t':
                 self.advance()
             elif self.current_char == '(':
                 tokens.append(Token(TT_LPAREN))
-                self.advance
-            if self.current_char in ' \t':
                 self.advance()
             elif self.current_char == ')':
                 tokens.append(Token(TT_RPAREN))
                 self.advance
-        return tokens
+            else:
+                char = self.current_char
+                self.advance()
+                return [], IllegalCharError("'" + char + "'")
+            
+        return tokens, None
 
     def make_number(self):
         num_str = ''
@@ -89,6 +103,13 @@ class Lexer:
         else:
             return Token(TT_FLOAT, float(num_str))
 
+#RUN
+
+def run(text):
+    lexer = Lexer(text)
+    tokens, error = lexer.make_tokens()
+
+    return tokens, error
 
 # Example usage
 if __name__ == "__main__":
